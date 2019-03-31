9361040b192b6a1afa92cc5ff0ce5468ccd6ba54
minho <radlohead@naver.com>
2019-03-31 22:59:46 +0900

    CRA spritesmith

---
+++ README.md
@@ -0,0 +1,3 @@
+# Create-React-App-Custom
+
+This is how to config sass and spritesmith with create-react-app.
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Sass Test</h2>
+        <h2>SpriteSmith Test</h2>
+        <div className="react-logo"></div>
+        <div className="redux-logo"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
